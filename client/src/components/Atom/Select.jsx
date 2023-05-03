9ff31e3060ba3cb895950f1d2d44956d5122e6a6
DAMC <david.moreno.cardenas@gmail.com>
2023-05-02 21:28:02 -0400

    actualizado el form

--- client/src/components/Atom/Select.jsx
+++ client/src/components/Atom/Select.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export default function Select (props){
+
     let options=props.arrayOptions;
+
     return(
-    <select onBlur={props.blur} ref={props.selectRef} className={props.style} id={props.id} onChange={props.select} size={props.size} key={props.id} multiple={props.multiple}>
-        <option value='' hidden>Choise...</option>
+    <select onBlur={props.blur} ref={props.selectRef} className={props.style} name={props.id} id={props.id} onChange={props.select} size={props.size} key={props.id} multiple={props.multiple}>
+        <option value="" hidden>Choise...</option>
         {options.map(c=><option className={props.styleOption} value={c} key={c}>{c}</option>)}
     </select>)
 }
